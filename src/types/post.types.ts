52cf560ed988e1009411c2721589c51e40c958a2
alxgrg <alxmgrg@gmail.com>
2024-05-14 02:28:45 -0400

    Factor out Post type and create blog post archive

---
+++ src/types/post.types.ts
@@ -0,0 +1,11 @@
+export interface Post {
+  title: string;
+  description: string;
+  author: string;
+  pubDate: string;
+  image?: {
+    url: string;
+    alt: string;
+  };
+  tags?: string[];
+}
