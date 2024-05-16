ac4011878f500c1b18694bb210c7d3f53beea0b0
alxgrg <alxmgrg@gmail.com>
2024-05-15 22:56:03 -0400

    Convert to content collections and  adjust types

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,16 +1,25 @@
 import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 import type { APIContext } from "astro";
+import { getCollection } from "astro:content";
 
 export async function GET({ site }: APIContext) {
   if (!site) {
     throw new Error("site address is missing");
   }
+
+  const posts = await getCollection("posts");
+
   return rss({
     title: "Astro Learner | Blog",
     description: "My journey learning Astro",
     site: site,
     // @ts-ignore type mismatch issue: https://github.com/withastro/docs/issues/6551
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
